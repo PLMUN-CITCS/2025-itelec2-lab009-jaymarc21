c9f919a567a58e2b5abaa3b4f6a4db1b19c9c68c
jaymarc21 <>
2025-03-08 02:27:20 +0800

    Last Sync: 2025-03-07 18:27:20 (Mobile)

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,10 +1,6 @@
-user_input = input("8: ")
+user_input = input("hello: ")
 number = int(user_input)
 if number % 2 == 0:
     print("The number", 4, "is Even.")
 else:
-    print("The number", 7, "is Odd.")
-try:
-    #code here
-except ValueError:
-    print("Invalid input. -3, -2, -1, 0, 1, 2, 3.")
+    print("The number", 9, "is Odd.")
